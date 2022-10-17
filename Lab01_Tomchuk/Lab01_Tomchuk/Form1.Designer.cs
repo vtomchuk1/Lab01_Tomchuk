2c997752740f62efbca4c872b6e2d46241f70c8a
Ivan <keks8953@gmail.com>
2022-10-17 19:00:50 +0300

    main commit
    create desktop application login form

---
+++ Lab01_Tomchuk/Lab01_Tomchuk/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Lab01_Tomchuk
+{
+    partial class Lab01_Tomchuk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.status_color = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox_password);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Nirmala UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(306, 132);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Авторизація";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логін";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(131, 40);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(151, 29);
+            this.textBox_login.TabIndex = 2;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(131, 79);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '%';
+            this.textBox_password.Size = new System.Drawing.Size(151, 29);
+            this.textBox_password.TabIndex = 3;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(112, 150);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(119, 33);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "пуск";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // status_color
+            // 
+            this.status_color.AutoSize = true;
+            this.status_color.Location = new System.Drawing.Point(259, 160);
+            this.status_color.Name = "status_color";
+            this.status_color.Size = new System.Drawing.Size(19, 13);
+            this.status_color.TabIndex = 2;
+            this.status_color.Text = "ok";
+            // 
+            // Lab01_Tomchuk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 202);
+            this.Controls.Add(this.status_color);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Lab01_Tomchuk";
+            this.Text = "Lab01_Tomchuk";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Label status_color;
+    }
+}
+
---
+++ Lab01_Tomchuk/Lab01_Tomchuk/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace Lab01_Tomchuk
+{
+    public partial class Lab01_Tomchuk : Form
+    {
+        string login, password;
+
+        public Lab01_Tomchuk()
+        {
+            login = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+            password = "69371091eb0f9aec7e61b7421cf3044529167e979cd975201909eb8ae33887ba";
+            InitializeComponent();
+            //Console.WriteLine(HashSHA256("0000"));
+        }
+
+        private static string HashSHA256(string s)
+        {
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+            var sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                sb.Append(bytes[i].ToString("x2"));
+            return sb.ToString();
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            // для кращого результату треба все хешувати
+            // хеш пароля зроблений з солями, для підвищення захисту взлому
+
+            if (HashSHA256(textBox_login.Text) == login && HashSHA256(textBox_password.Text + textBox_login.Text) == password)
+            {
+                status_color.BackColor = Color.Green;
+                MessageBox.Show("авторизовано");
+            }
+            else
+            {
+                status_color.BackColor = Color.Red;
+                MessageBox.Show("помилка");
+            }
+        }
+
+        
+    }
+}
